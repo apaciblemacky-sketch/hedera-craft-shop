c239298745f7546af44a91c9e05f6e653dc7d8c9
apaciblemacky-sketch <apaciblemacky@gmail.com>
2026-08-22 08:58:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hedera-secure-secret-key-2026'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crafts.db'
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///crafts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
